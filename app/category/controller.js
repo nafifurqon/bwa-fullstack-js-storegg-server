f7c965e7f02b690a2223deb256ae9231974cf19d
nafifurqon <muhammadnafi98@gmail.com>
2021-12-12 20:52:03 +0700

    feat: create function, models, and view to display form add category and save category to db

--- app/category/controller.js
+++ app/category/controller.js
@@ -1,7 +1,28 @@
+const Category = require('./model');
+
 module.exports = {
   index: async (req, res) => {
     try {
       res.render('admin/category/view_category');
+    } catch (error) {
+      console.log(`error`, error);
+    }
+  },
+  viewCreate: async (req, res) => {
+    try {
+      res.render('admin/category/create');
+    } catch (error) {
+      console.log(`error`, error);
+    }
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const { name } = req.body;
+
+      let category = await Category({ name });
+      await category.save();
+
+      res.redirect('/category');
     } catch (error) {
       console.log(`error`, error);
     }
