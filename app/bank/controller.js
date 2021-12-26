5c524522a18bad14ae51af3561c42899d6985bca
nafifurqon <muhammadnafi98@gmail.com>
2021-12-26 21:26:17 +0700

    feat: created endpoint and function to delete data bank

--- app/bank/controller.js
+++ app/bank/controller.js
@@ -80,20 +80,20 @@ module.exports = {
       res.redirect('/bank');
     }
   },
-  // actionDelete: async (req, res) => {
-  //   try {
-  //     const { id } = req.params;
+  actionDelete: async (req, res) => {
+    try {
+      const { id } = req.params;
 
-  //     await Nominal.findByIdAndRemove({ _id: id });
+      await Bank.findByIdAndRemove({ _id: id });
 
-  //     req.flash('alertMessage', 'Berhasil hapus nominal');
-  //     req.flash('alertStatus', 'success');
+      req.flash('alertMessage', 'Berhasil hapus bank');
+      req.flash('alertStatus', 'success');
 
-  //     res.redirect('/nominal');
-  //   } catch (error) {
-  //     req.flash('alertMessage', error.message);
-  //     req.flash('alertStatus', 'danger');
-  //     res.redirect('/nominal');
-  //   }
-  // },
+      res.redirect('/bank');
+    } catch (error) {
+      req.flash('alertMessage', error.message);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/bank');
+    }
+  },
 };
