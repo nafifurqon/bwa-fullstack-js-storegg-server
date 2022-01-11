662cce96412e6d046d899e35bbc6554d2cf01cb6
nafifurqon <muhammadnafi98@gmail.com>
2022-01-11 21:51:06 +0700

    feat: created function to handle approve or decline transaction

--- app/transaction/router.js
+++ app/transaction/router.js
@@ -2,22 +2,12 @@ const express = require('express');
 const router = express.Router();
 const {
   index,
-  // viewCreate,
-  // actionCreate,
-  // viewEdit,
-  // actionEdit,
-  // actionDelete,
-  // actionStatus,
+  actionStatus,
 } = require('./controller');
 const { isLoginAdmin } = require('../middleware/auth');
 
 router.use(isLoginAdmin);
 router.get('/', index);
-// router.get('/create', viewCreate);
-// router.post('/create', actionCreate);
-// router.get('/edit/:id', viewEdit);
-// router.put('/edit/:id', actionEdit);
-// router.delete('/delete/:id', actionDelete);
-// router.put('/status/:id', actionStatus);
+router.put('/status/:id', actionStatus);
 
 module.exports = router;
