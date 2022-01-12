4bc606bda592638e121bfce9e7eaa22f4f071abd
nafifurqon <muhammadnafi98@gmail.com>
2022-01-12 21:12:05 +0700

    feat: created detail page API

--- app/player/controller.js
+++ app/player/controller.js
@@ -12,7 +12,28 @@ module.exports = {
     } catch (error) {
       res
         .status(500)
-        .json({ message: error.message || 'Terjadi kesalahan pada server' });
+        .json({ message: error.message || 'Internal Server Error' });
+    }
+  },
+  detailPage: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const vouchers = await Voucher.findOne({ _id: id })
+        .populate('category')
+        .populate('nominals')
+        .populate('user', '_id name phoneNumber');
+
+      if (!vouchers) {
+        return res
+          .status(404)
+          .json({ message: 'Voucher game tidak ditemukan' });
+      }
+
+      res.status(200).json({ data: vouchers });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: error.message || 'Internal Server Error' });
     }
   },
 };
--- app/player/router.js
+++ app/player/router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { landingPage } = require('./controller')
+const { landingPage, detailPage } = require('./controller')
 
 router.get('/landingpage', landingPage);
+router.get('/:id/detail', detailPage);
 
 module.exports = router;
