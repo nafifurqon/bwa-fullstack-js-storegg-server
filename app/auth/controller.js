6f232eac6faf063c044d5adab3821d1bee296d9b
nafifurqon <muhammadnafi98@gmail.com>
2022-01-30 18:42:31 +0700

    feat: craeted API for sign in

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -2,6 +2,8 @@ const Player = require('../player/model');
 const path = require('path');
 const fs = require('fs');
 const config = require('../../config');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
   signup: async (req, res, next) => {
@@ -66,5 +68,56 @@ module.exports = {
       }
       next(error);
     }
+  },
+  signin: async (req, res, next) => {
+    const { email, password } = req.body;
+
+    console.log('req.body', req.body);
+
+    Player.findOne({ email: email })
+      .then((player) => console.log('player', player))
+      .catch((error) => console.log('error', error));
+
+    Player.findOne({ email: email })
+      .then((player) => {
+        if (player) {
+          const checkPassword = bcrypt.compareSync(password, player.password);
+
+          if (checkPassword) {
+            const token = jwt.sign(
+              {
+                player: {
+                  id: player.id,
+                  username: player.username,
+                  email: player.email,
+                  name: player.name,
+                  phoneNumber: player.phoneNumber,
+                  avatar: player.avatar,
+                },
+              },
+              config.jwtKey
+            );
+
+            res.status(200).json({
+              data: { token },
+            });
+          } else {
+            res.status(403).json({
+              message: 'kata sandi yang anda masukkan salah.',
+            });
+          }
+        } else {
+          res.status(403).json({
+            message: 'email yang anda masukkan belum terdaftar.',
+          });
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({
+          message: error.message || 'Internal Server Error',
+        });
+
+        next();
+      });
   },
 };
