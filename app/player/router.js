fa494b6ed942137c43cceaa85a758fdb585db546
nafifurqon <muhammadnafi98@gmail.com>
2022-02-03 07:59:09 +0700

    feat: created API to get transaction history

--- app/player/router.js
+++ app/player/router.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { landingPage, detailPage, category, checkout } = require('./controller')
+const { landingPage, detailPage, category, checkout, history } = require('./controller')
 const { isLoginPlayer } = require('../middleware/auth');
 
 router.get('/landingpage', landingPage);
 router.get('/:id/detail', detailPage);
 router.get('/category', category);
 router.post('/checkout', isLoginPlayer, checkout);
+router.get('/history', isLoginPlayer, history);
 
 module.exports = router;
